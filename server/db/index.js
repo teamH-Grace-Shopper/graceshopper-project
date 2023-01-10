b4b80742dce37feb3500798be24177a32b3b12e3
aguilana <naguila.mandich@gmail.com>
2023-01-10 16:41:21 -0500

    #3 associations finished

--- server/db/index.js
+++ server/db/index.js
@@ -5,6 +5,7 @@ const db = require('./db');
 const User = require('./models/User');
 const Product = require('./models/Product');
 const Order = require('./models/Order');
+const Cart = require('./models/Cart')
 
 // ASSOCIATIONS
 
@@ -14,13 +15,30 @@ Product.belongsTo(User);
 
 // User and Order - one-to-many
 User.hasMany(Order);
-Order.belongsTo(User)
+Order.belongsTo(User);
+
+// User and Cart - one-to-one
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+/* 
+ * Junction Tables Created by Sequelize Associations 
+ */
+
+// Cart and Product - many-to-many
+Cart.belongsToMany(Product, { through: "CartItem" });
+Product.belongsToMany(Cart, { through: "CartItem" });
+
+// Order and Product - many-to-many
+Order.belongsToMany(Product, { through: "OrderItem"});
+Product.belongsToMany(Product, { through: "OrderItem"})
 
 module.exports = {
   db,
   models: {
     User,
     Product,
-    Order
+    Order,
+    Cart
   },
 }
