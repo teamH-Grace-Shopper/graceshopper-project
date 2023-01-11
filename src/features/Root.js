6c100c6e7a2af83f7e2ec3d915de3db7efd0d5a5
Sarah Hightower <sightower@gmail.com>
2023-01-11 08:16:56 -0800

    Test

---
+++ src/features/Root.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
+
+const Root = () => {
+    return (
+        <BrowserRouter>
+        <div>
+            
+        </div>
+        </BrowserRouter>
+    )
+}
+
+export default Root
