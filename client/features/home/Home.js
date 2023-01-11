9b1813955286c74ef3ed3d01a63b50a4adf4cf36
Sarah Hightower <sightower@gmail.com>
2023-01-11 12:58:12 -0800

    NavBar

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -8,8 +8,8 @@ const Home = (props) => {
   const username = useSelector((state) => state.auth.me.username);
 
   return (
-    <div>
-      <h3>Welcome, {username}</h3>
+    <div id="welcome-box">
+      <h3>Welcome {username} !</h3>
     </div>
   );
 };
