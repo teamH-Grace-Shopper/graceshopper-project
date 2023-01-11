8bcd2a847aeafa9ec9fe64dd162837ac66ce0c26
aguilana <naguila.mandich@gmail.com>
2023-01-10 20:50:46 -0500

    re-did db models and associations

--- server/db/index.js
+++ server/db/index.js
@@ -5,35 +5,21 @@ const db = require('./db');
 const User = require('./models/User');
 const Product = require('./models/Product');
 const Order = require('./models/Order');
-const Cart = require('./models/Cart')
-const CartItem = require('./models/CartItem')
 const OrderItem = require('./models/OrderItem')
 
 // ASSOCIATIONS
 
 // User and Product - one-to-many - need to figure this out
-User.hasMany(Product); 
-Product.belongsTo(User);
+
 
 // User and Order - one-to-many
 User.hasMany(Order);
 Order.belongsTo(User);
 
-// User and Cart - one-to-one
-User.hasOne(Cart);
-Cart.belongsTo(User);
+OrderItem.belongsTo(Order)
+Order.hasMany(OrderItem)
 
-/* 
- * Junction Tables Created by Sequelize Associations 
- */
-
-// Cart and Product - many-to-many
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-
-// Order and Product - many-to-many
-Order.belongsToMany(Product, { through: OrderItem});
-Product.belongsToMany(Order, { through: OrderItem});
+OrderItem.belongsTo(Product)
 
 module.exports = {
   db,
@@ -41,6 +27,6 @@ module.exports = {
     User,
     Product,
     Order,
-    Cart,
+    OrderItem,
   },
 }
--- server/db/models/OrderItem.js
+++ server/db/models/OrderItem.js
@@ -1,10 +1,16 @@
 const Sequelize = require('sequelize');
-const { INTEGER } = Sequelize
+const { INTEGER, DECIMAL } = Sequelize
 const db = require('../db');
 
-const OrderItem = db.define('order', {
+const OrderItem = db.define('orderItem', {
     quantity: {
-        type: INTEGER
+        type: INTEGER,
+        allowNull: false,
+        defaultValue: 1
+    },
+    price: {
+        type: DECIMAL,
+        allowNull: false
     }
 })
 
