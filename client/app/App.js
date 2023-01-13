6cd7733d9f9d3b2b6e81f3d0fef443e82b818635
Sarah Hightower <sightower@gmail.com>
2023-01-13 07:43:10 -0800

    fixed footer import

--- client/app/App.js
+++ client/app/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import Navbar from '../features/navbar/Navbar';
 import Sidebar from '../features/navbar/SideNav'
-import Footer from '../../src/features/footer';
+import Footer from '../features/footer/footer';
 import MainPage from '../features/home/MainPage';
 import ProductRoutes from './ProductRoutes';
 
