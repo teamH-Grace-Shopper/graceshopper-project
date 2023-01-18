3472185b1ec4984b3ef7db7c8f57661a387d4957
aguilana <naguila.mandich@gmail.com>
2023-01-18 18:56:26 -0500

    image fix

--- client/features/cart/Cart.js
+++ client/features/cart/Cart.js
@@ -128,7 +128,7 @@ const Cart = ({ isLoggedIn }) => {
                   return (
                     <div className="cart-item" key={order.id}>
                       <div className="cart-product">
-                        <img src="https://images.unsplash.com/photo-1531318701087-32c11653dd77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+                        <img src={order.imageUrl} />
                         <div>
                           <h3>{order.name}</h3>
                           <button
